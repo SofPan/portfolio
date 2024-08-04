3831808ea5feca63b69b9e74f9a7b7ab8977a2dd
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-08-04 11:05:04 -0400

    hide About section, potentially removing permanently

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './styles.css';
 import Header from './components/Header/Header';
 import Projects from './components/Projects/Projects';
 import Contact from './components/ContactSection/Contact';
-import About from './components/About';
+// import About from './components/About';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <main>
         <Projects />
         <Contact />
-        <About />
+        {/* <About /> */}
       </main>
     </div>
   );
