281f2a1b5d4a283d3b05399aa6036bfce88c8cc2
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-08-05 15:54:51 -0400

    replace placeholder text with MUI icons for socials

--- src/components/ContactSection/Socials.jsx
+++ src/components/ContactSection/Socials.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 const Socials = () => {
   return(
-    <>
-      <h3>Find me on: </h3>
-      <ul>
-        <li>LinkedIn</li>
-        <li>GitHub</li>
-        <li>Instagram</li>
-        <li>TikTok</li>
+    <aside className="socials flex flex-column">
+      <h3>Follow Me: </h3>
+      <ul className="flex socials-list">
+        <li><a href="#LinkedIn"><LinkedInIcon /></a></li>
+        <li><a href="#GitHub"><GitHubIcon /></a></li>
+        <li><a href="#Instagram"><InstagramIcon /></a></li>
+        <li><a href="#TikTok">X</a></li>
       </ul>
-    </>
+    </aside>
   );
 }
 
