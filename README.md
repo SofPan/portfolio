f9608e48f559d63d471bbceeb1f1af770f057c6f
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-08-05 19:07:16 -0400

    import dummy data to lists and apply styles

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## Projects Page
 - Full list of projects using Github api combined with links and logos
+  - Set up Github API
+  - Determine how to pull out technologies used for skills
+  - Determine how to use thumbnail image or some sort of image
+  - Determine how to get description
 
 ## Contact Page
 - Contact form sends through to my primary email
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,11 +1,58 @@
 import { NavLink } from "react-router-dom";
+import Projects from "../components/Projects/Projects";
+import Socials from "../components/ContactSection/Socials";
+
+const DUMMY_PROJECTS = [
+  {
+    id: 1,
+    title: "Title 1",
+    description: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.",
+    skills: ["react", "ruby", "javascript"],
+    thumbnail: "https://picsum.photos/200"
+  },
+  {
+    id: 2,
+    title: "Title 2",
+    description: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.",
+    skills: ["react", "ruby", "javascript"],
+    thumbnail: "https://picsum.photos/300"
+  },
+  {
+    id: 3,
+    title: "Title 3",
+    description: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.",
+    skills: ["react", "ruby", "javascript"],
+    thumbnail: "https://picsum.photos/400"
+  }
+]
 
 const Work = () => {
   return(
-    <>
-      <NavLink to="/" className="btn btn-primary">Home</NavLink>
-      <p>Projects go here!</p>
-    </>
+    <div className="bg-alt">
+      <header>
+        <NavLink to="/" className="btn btn-primary">Home</NavLink>
+      </header>
+      <main>
+        <section>
+          <h2>Github</h2>
+          <Projects listItems={DUMMY_PROJECTS} />
+        </section>
+        <section>
+          <h2>Websites</h2>
+          <Projects listItems={DUMMY_PROJECTS} />
+        </section>
+        <section>
+          <h2>Testimonials</h2>
+          <Projects listItems={DUMMY_PROJECTS} />
+        </section>
+      </main>
+      <footer className="bg-main scalloped ">
+        <div className="footer-inner flex justify-between">
+          <Socials />
+          <NavLink to="/contact" className="btn btn-primary">Contact Me</NavLink>
+        </div>
+      </footer>
+    </div>
   )
 }
 
