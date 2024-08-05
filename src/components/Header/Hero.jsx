99dc928692b13e30194e544754b317d70519430f
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-08-05 18:07:16 -0400

    add header paragraph and finalize styles

--- src/components/Header/Hero.jsx
+++ src/components/Header/Hero.jsx
@@ -5,19 +5,26 @@ import Socials from "../ContactSection/Socials";
 const Hero = () => {
   return(
     <div className="hero bg-alt">
-      <header className="hero-header flex flex-column">
-        <div>
-          <h1>
-            I'm <span className="color-dark">Sofia</span><br />
-            <span class="small">
-              A <span className="color-dark">Freelance Web Developer</span>
-            </span>
-          </h1>
-        </div>
-        <div>
-          <button className="btn btn-primary">My Work</button>
-          <button className="btn btn-primary">Contact Me</button>
-        </div>
+      <header className="hero-header">
+        <aside>
+          <div>
+            <h1>
+              I'm <span className="color-dark">Sofia</span><br />
+              <span class="small">
+                A <span className="color-dark">Freelance Web Developer</span>
+              </span>
+            </h1>
+          </div>
+          <div>
+            <button className="btn btn-primary">My Work</button>
+            <button className="btn btn-primary">Contact Me</button>
+          </div>
+        </aside>
+        <article className="hero-text-block">
+          <p>
+            With over <span className="color-dark">5 years </span> of experience across the fullstack, I can bring <span className="color-dark">innovation</span>, and <span className="color-dark">polish</span> to your project. Reach out today so we can arrange a meeting!
+          </p>
+        </article>
       </header>
       <footer className="hero-footer">
         <Socials />
