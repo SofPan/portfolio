a4255b946ce950ed676a2b45d1a4737ba6f53b64
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-08-15 11:23:25 -0400

    prepare Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,25 @@
 import { NavLink } from "react-router-dom";
+import Socials from "../components/ContactSection/Socials";
 
 const Contact = () => {
   return(
     <>
-      <NavLink to="/" className="btn btn-primary">Home</NavLink>
-      <p>Contact Form Goes Here!</p>
+      <div className="page_Work bg-alt full-height">
+        <header>
+          <NavLink to="/" className="btn btn-primary">Home</NavLink>
+        </header>
+        <main>
+          <section>
+            <h2>Contact Me</h2>
+          </section>
+        </main>
+      </div>
+      <footer className="bg-main scalloped ">
+        <div className="footer-inner flex justify-between">
+          <Socials />
+          <NavLink to="/contact" className="btn btn-primary">My Work</NavLink>
+        </div>
+      </footer>
     </>
   )
 }
